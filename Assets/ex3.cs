798cb48152e0933a6749224af57fac214a7dda9c
Yogabharathi3 <118899387+Yogabharathi3@users.noreply.github.com>
2024-04-13 11:38:54 +0530

    Add files via upload

---
+++ Assets/ex3.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor;
+using UnityEngine;
+
+
+public class ex3 : MonoBehaviour
+{
+    public float speed;
+    public float jumpforce;
+    private Rigidbody2D rb;
+    public Score cc;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(moveInput, 0, 0) * speed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space) && Mathf.Abs(rb.velocity.y) < 0.001f)
+        {
+            rb.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Destroy"))
+        {
+            cc.coincount++;
+            Destroy(other.gameObject);
+        }
+    }
+}
